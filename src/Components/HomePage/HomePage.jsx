59f84929e0da37f1636c8b659c57d5acfefc1551
priyankanair <priyankanair@vt.edu>
2023-11-11 00:27:14 -0500

    Most of the front-end skeleton

---
+++ src/Components/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div className="homepage">
+            <h1>Virginia Tech</h1>
+            <div className="buttons">
+                <button className="login-signup">Login/Sign Up</button>
+                <button className="about">About</button>
+            </div>
+        </div>
+    );
+};
+
+export default Homepage;
