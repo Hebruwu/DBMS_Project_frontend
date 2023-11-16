09edc9f891d8cfcb674ee3bddc836b6a6450f9a0
Sparsh Joshi <sparshjoshi1998@vt.edu>
2023-11-15 23:46:48 -0500

    Add authetication endpoints

--- admin_blueprint.py
+++ admin_blueprint.py
@@ -1,7 +1,7 @@
 import traceback
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint, Response, jsonify, request
 from dataclasses import asdict
-from database.eventhosting import get_engine, get_admin
+from database.eventhosting import get_engine, get_admin, authenticate_admin
 
 admin_routes = Blueprint('admin_routes', __name__)
 
@@ -11,5 +11,16 @@ def index(username: str) -> Response:
         engine = get_engine()
         admin = get_admin(engine, username)
         return asdict(admin)
+    except Exception as e:
+        return jsonify(error=traceback.format_exc()),500
+
+@admin_routes.route("authenticate", methods=["GET"])
+def authenitcate() -> Response:
+    try:
+        engine = get_engine()
+        auth_args = request.json
+        authenitcated = authenticate_admin(engine, auth_args["username"], auth_args["password"])
+        return jsonify(is_authenticated=authenitcated), (200 if authenitcated \
+        else 401)
     except Exception as e:
         return jsonify(error=traceback.format_exc()),500
--- student_blueprint.py
+++ student_blueprint.py
@@ -1,7 +1,7 @@
 import traceback
-from flask import Blueprint, Response, jsonify
+from flask import Blueprint, Response, jsonify, request
 from dataclasses import asdict
-from database.eventhosting import get_engine, get_student
+from database.eventhosting import get_engine, get_student, authenticate_student
 
 student_routes = Blueprint('student_routes', __name__)
 
@@ -11,5 +11,16 @@ def index(username: str) -> Response:
         engine = get_engine()
         student = get_student(engine, username)
         return asdict(student)
+    except Exception as e:
+        return jsonify(error=traceback.format_exc()),500
+    
+@student_routes.route("authenticate", methods=["GET"])
+def authenitcate() -> Response:
+    try:
+        engine = get_engine()
+        auth_args = request.json
+        authenitcated = authenticate_student(engine, auth_args["username"], auth_args["password"])
+        return jsonify(is_authenticated=authenitcated), (200 if authenitcated \
+        else 401)
     except Exception as e:
         return jsonify(error=traceback.format_exc()),500
