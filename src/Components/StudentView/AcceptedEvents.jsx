dba4082804f2cc499b33020a08b1f85ea86928c4
Daniel Sabanov <daniel@sadogursky.com>
2023-11-29 13:52:17 -0500

    Fetch data for events on the student side.

--- src/Components/StudentView/AcceptedEvents.jsx
+++ src/Components/StudentView/AcceptedEvents.jsx
@@ -1,6 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './AcceptedEvents.css'; // You can create a CSS file for styling
 import {useNavigate} from 'react-router-dom';
+
+function Event({date, description, event_type, location, modality, name}) {
+    return (
+        <div>
+            Date: {date}
+        </div>
+    )
+}
+
+function EventList() {
+    const [events, setEvents] = useState([])
+    useEffect(() => {
+        async function fetchEvents() {
+            try {
+                let eventRes = await fetch("http://ec2-18-118-164-236.us-east-2.compute.amazonaws.com/eventhosting/events/events_details",
+                    {
+                        headers: {"Content-Type": "application/json",}
+                    })
+                let eventJson = await eventRes.json();
+                setEvents(eventJson)
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
+        fetchEvents()
+    }, []);
+    console.log(events)
+    return (
+        <>
+            {events.map((item, idx) => (
+                <Event
+                    key={idx} // Add a key prop for each item in the list
+                    date={item.DATE_FROM}
+                    description={item.DESCRIPTION}
+                    event_type={item.EVENT_TYPE}
+                    location={item.LOCATION}
+                    modality={item.MODALITY}
+                    name={item.NAME}
+                />
+            ))}
+        </>
+    )
+}
 
 const AcceptedEvents = () => {
     const navigate = useNavigate();
@@ -19,6 +64,7 @@ const AcceptedEvents = () => {
             </div>
             <div className="header" style={{marginLeft: -10, marginTop: 30}}>
                 <h2>Events that I'm Invited to...</h2>
+                <EventList/>
             </div>
             <button style={{marginTop: 50}} onClick={handleLogoutButton} className="button">Logout</button>
         </div>
